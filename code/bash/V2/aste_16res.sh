0899b8e4163ebfe33ba24fb2aa4b4a39195cda81
Jiang Baoxing <68661161+BaoSir529@users.noreply.github.com>
2023-06-15 22:34:34 +0800

    Update aste_16res.sh

--- code/bash/V2/aste_16res.sh
+++ code/bash/V2/aste_16res.sh
@@ -1,1 +1,1 @@
-bash ../bash/aste.sh -s 4113826069 -c 0 -d V2/16res -l 3 -t resnet -n 2 -z 0.3 -e tensorcontext -D 64
+bash ../aste.sh -s 4113826069 -c 0 -d V2/16res -l 3 -t resnet -n 2 -z 0.3 -e tensorcontext -D 64
